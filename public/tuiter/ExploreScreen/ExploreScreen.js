4fad97e8ce3f8cfad415c1dadc16cca8b5993de4
Rishita <reddy.ri@northeastern.edu>
2022-02-27 23:26:26 -0500

    a5 submission

--- public/tuiter/ExploreScreen/ExploreScreen.js
+++ public/tuiter/ExploreScreen/ExploreScreen.js
@@ -1,7 +1,6 @@
 import NavigationSidebar from "../NavigationSidebar/index.js";
 import ExploreComponent from "./ExploreComponent.js";
 import WhoToFollowList from "../WhoToFollowList/index.js";
-import PostSummaryList from "../PostSummaryList/index.js";
 
 (function ($) {
     $('#wd-explore').append(`
